54ea20d9e5c16e780040d5eb5e201155e1f5e922
practicasTicGon <ticpracticas@asysgon.com>
2024-05-16 19:26:12 +0200

    [ADD] adding fx implementation

--- src/Main2.java
+++ src/Main2.java
@@ -1,39 +1,29 @@
+import awt.scene.SceneWindow;
 import javafx.application.Application;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+
 import javafx.scene.layout.StackPane;
+
 import javafx.stage.Stage;
+
 
 public class Main2 extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
-        // Create a Button
-        Button btn = new Button();
-        btn.setText("Click me!");
-
-        // Set action for the Button
-        btn.setOnAction(event -> System.out.println("Hello, JavaFX!"));
-
-        // Add the Button to a layout (StackPane)
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        // Create a Scene
-        Scene scene = new Scene(root, 300, 250);
-
-        // Set the Scene on the Stage
-        primaryStage.setScene(scene);
-
-        // Set the title of the Stage
-        primaryStage.setTitle("JavaFX Example");
-
-        // Show the Stage
+    public void start(Stage primaryStage) throws Exception {
+        // Load the FXML file
+        Parent  root = FXMLLoader.load(getClass().getResource("fx/sample.fxml"));
+        primaryStage.setScene(new Scene(root, 600, 400));
+        // Set the stage title
+        primaryStage.setTitle("JavaFX App");
+        // Show the stage
         primaryStage.show();
     }
 
     public static void main(String[] args) {
-        // Launch the JavaFX application
         launch(args);
     }
 }
---
+++ src/start.sh
@@ -0,0 +1,2 @@
+javac --module-path /opt/PolyRender/javafx-sdk-21.0.2/lib --add-modules javafx.controls,javafx.fxml,javafx.web,javafx.swing,javafx.media,javafx.graphics,javafx.base Main2.java
+java --module-path /opt/PolyRender/javafx-sdk-21.0.2/lib --add-modules javafx.controls,javafx.fxml,javafx.web,javafx.swing,javafx.media,javafx.graphics,javafx.base Main2.java
